a48f81dc17bde4c6c18e4085f33b937b88bfbbaf
Prince Dale Limosnero <princedalelimosnero@gmail.com>
2026-08-23 18:05:41 +0800

    Inject Buffer and process polyfills into Vite entrypoint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,9 @@
+﻿import { Buffer } from 'buffer'
+window.Buffer = window.Buffer || Buffer
+if (typeof window.process === 'undefined') {
+  window.process = { env: {} } as any
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
